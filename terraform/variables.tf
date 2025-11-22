bf842abad39e2956489f73a20ac626617b2309b7
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-11-22 19:13:57 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,42 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default = [
+    "10.0.0.0/20",
+    "10.0.16.0/20",
+    "10.0.32.0/20"
+  ]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default = [
+    "10.0.128.0/20",
+    "10.0.144.0/20",
+    "10.0.160.0/20"
+  ]
+}
+
+variable "azs" {
+  description = "Availability zones for subnets"
+  type        = list(string)
+  default = [
+    "ap-south-1a",
+    "ap-south-1b",
+    "ap-south-1c"
+  ]
+}
 
