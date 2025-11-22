ca2d52eaaa6451cc63360a8c78da30271b4d4125
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-11-22 19:15:00 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,1 +1,22 @@
+############################
+# VPC CREATION
+############################
+
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "hyp-minds-dev-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "hyp-minds-dev-igw"
+  }
+}
 
