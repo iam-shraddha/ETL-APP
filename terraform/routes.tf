a3d6d65bf7b710e3092505af84adff4e8c235b59
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-11-22 19:16:08 +0530

    Update routes.tf

--- terraform/routes.tf
+++ terraform/routes.tf
@@ -1,1 +1,47 @@
 
+############################
+# PUBLIC ROUTE TABLE
+############################
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  # Public subnets get internet access through IGW
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "hyp-minds-dev-rtb-public"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+############################
+# PRIVATE ROUTE TABLES (NO INTERNET)
+############################
+
+resource "aws_route_table" "private" {
+  for_each = aws_subnet.private
+
+  vpc_id = aws_vpc.this.id
+
+  # No default route, these subnets stay isolated
+  tags = {
+    Name = "hyp-minds-dev-rtb-private${each.key}"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private[each.key].id
+}
