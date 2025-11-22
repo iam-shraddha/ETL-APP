e45d5a3893eb1a6a280e9e70865e9e6ca0091e89
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-11-22 19:15:33 +0530

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,1 +1,34 @@
+############################
+# PUBLIC SUBNETS
+############################
+
+resource "aws_subnet" "public" {
+  for_each = { for idx, cidr in var.public_subnets : idx => cidr }
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = each.value
+  availability_zone       = var.azs[tonumber(each.key)]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "hyp-minds-dev-subnet-public${tonumber(each.key)+1}-${var.azs[tonumber(each.key)]}"
+  }
+}
+
+############################
+# PRIVATE SUBNETS
+############################
+
+resource "aws_subnet" "private" {
+  for_each = { for idx, cidr in var.private_subnets : idx => cidr }
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = each.value
+  availability_zone       = var.azs[tonumber(each.key)]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "hyp-minds-dev-subnet-private${tonumber(each.key)+1}-${var.azs[tonumber(each.key)]}"
+  }
+}
 
