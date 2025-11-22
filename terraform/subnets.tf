dcfed9702f70cd4da4c517b723e9d29a03e731bc
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-11-22 19:28:06 +0530

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -3,7 +3,10 @@
 ############################
 
 resource "aws_subnet" "public" {
-  for_each = { for idx, cidr in var.public_subnets : idx => cidr }
+  for_each = {
+    for idx, cidr in var.public_subnets :
+    idx => cidr
+  }
 
   vpc_id                  = aws_vpc.this.id
   cidr_block              = each.value
@@ -11,7 +14,7 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "hyp-minds-dev-subnet-public${tonumber(each.key)+1}-${var.azs[tonumber(each.key)]}"
+    Name = "hyp-minds-dev-subnet-public${tonumber(each.key) + 1}-${var.azs[tonumber(each.key)]}"
   }
 }
 
@@ -20,7 +23,10 @@ resource "aws_subnet" "public" {
 ############################
 
 resource "aws_subnet" "private" {
-  for_each = { for idx, cidr in var.private_subnets : idx => cidr }
+  for_each = {
+    for idx, cidr in var.private_subnets :
+    idx => cidr
+  }
 
   vpc_id                  = aws_vpc.this.id
   cidr_block              = each.value
@@ -28,7 +34,6 @@ resource "aws_subnet" "private" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "hyp-minds-dev-subnet-private${tonumber(each.key)+1}-${var.azs[tonumber(each.key)]}"
+    Name = "hyp-minds-dev-subnet-private${tonumber(each.key) + 1}-${var.azs[tonumber(each.key)]}"
   }
 }
-
