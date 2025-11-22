3559cab1fb7f6a38e0cbb68fea554148b98a13ba
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-11-22 19:14:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,18 @@
+############################
+# MAIN TERRAFORM ENTRYPOINT
+############################
+
+# This file doesn't need many resources.
+# It just loads all other .tf files in this folder automatically.
+
+terraform {
+  required_version = ">= 1.2.0"
+}
+
+# Provider block is already in providers.tf
+# Variables are in variables.tf
+# VPC, Subnets, SG, Routes are in their respective files.
+
+# You can leave this file empty,
+# or add future configuration like remote backend.
 
